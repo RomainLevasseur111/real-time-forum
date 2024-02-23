ba5930431ba8685c01a129417bb8a49674da10e4
Simon Lecureux <lecureuxsimon@orange.fr>
2024-02-23 10:35:33 +0100

    feat(real-time-forum): websocket chat

--- main.go
+++ main.go
@@ -3,6 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	clients  []*websocket.Conn
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
 )
 
 func main() {
@@ -11,6 +21,30 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	fmt.Println("Server starting...\nServer hosted at http://localhost:8989/")
+
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil) // error ignored for simplicity
+		if err != nil {
+			panic(err)
+		}
+
+		clients = append(clients, conn)
+
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			for _, client := range clients {
+				if err = client.WriteMessage(msgType, msg); err != nil {
+					return
+				}
+			}
+		}
+	})
 
 	http.HandleFunc("/", Server)
 	if err := http.ListenAndServe(":8989", nil); err != nil {
--- server.go
+++ server.go
@@ -3,4 +3,5 @@ package main
 import "net/http"
 
 func Server(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./templates/index.html")
 }
