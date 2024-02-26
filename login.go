0068ef65c2d699c1d329b3ca9a5a267782277171
rlevasse <romainlevasseur4@gmail.com>
2024-02-26 18:59:01 +0100

    feat: login function

--- login.go
+++ login.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	"golang.org/x/crypto/bcrypt"
@@ -8,14 +10,50 @@ import (
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	// Check login datas in the database
-
-	// If authentication is successful:
-	GiveCookie(w, "nickname here")
-
-	// Redirect to another page
+	if r.Method != "POST" {
+		// error 405 method not allowed
+		return
+	}
+	db, err := sql.Open(DRIVER, DB)
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+	defer db.Close()
+	var pass string
+	name := r.FormValue("login")
+	pwd := r.FormValue("password")
+	if !EmailAlreadyExist(name) {
+		rows, err := db.Query("SELECT nickname FROM USERS WHERE email = ?",name)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for rows.Next() {
+			rows.Scan(&name)
+		}
+	}
+	if !NicknameAlreadyExists(name){
+		rows, err := db.Query("SELECT password FROM USERS WHERE nickname = ?",name)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for rows.Next() {
+			rows.Scan(&pass)
+		}	
+	}
+	if CheckPasswordHash(pwd, pass){
+		GiveCookie(w, name)
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+	}else{
+		fmt.Println("marchpa")
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+	}
 }
 
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
+
